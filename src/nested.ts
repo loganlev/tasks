300350b377b1122144b9e934a8d59a769a89517f
Logan Levine <loganlev@udel.edu>
2023-03-06 16:47:48 -0500

    fix

--- src/nested.ts
+++ src/nested.ts
@@ -1,5 +1,3 @@
-import { Options } from "prettier";
-import Q from "q";
 import { Answer } from "./interfaces/answer";
 import { Question, QuestionType } from "./interfaces/question";
 import { duplicateQuestion, makeBlankQuestion } from "./objects";
