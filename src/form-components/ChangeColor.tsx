895c4915fb672c290daf8879a1798400bee41488
Logan Levine <loganlev@udel.edu>
2023-03-15 19:21:26 -0400

    Task 10

--- src/form-components/ChangeColor.tsx
+++ src/form-components/ChangeColor.tsx
@@ -1,9 +1,61 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function ChangeColor(): JSX.Element {
+    const COLORS = [
+        "Red",
+        "Orange",
+        "Yellow",
+        "Green",
+        "Blue",
+        "Purple",
+        "Pink",
+        "Brown"
+    ];
+
+    const DEFAULT_COLOR = COLORS[0];
+
+    const [color, setColor] = useState<string>(DEFAULT_COLOR);
+
+    function updateColor(event: React.ChangeEvent<HTMLInputElement>) {
+        setColor(event.target.value);
+    }
+
     return (
         <div>
-            <h3>Change Color</h3>
+            <div>
+                <h3>Change Color</h3>
+            </div>
+            {COLORS.map((color: string) => (
+                <Form.Check
+                    inline
+                    key={color}
+                    type="radio"
+                    name="colors"
+                    onChange={updateColor}
+                    id={color}
+                    label={color}
+                    value={color}
+                    checked={false}
+                    style={{
+                        backgroundColor: color
+                    }}
+                />
+            ))}
+            <div>
+                You have chosen{" "}
+                <span
+                    data-testid="colored-box"
+                    style={{
+                        width: "100%",
+                        height: "10%",
+                        backgroundColor: color
+                    }}
+                >
+                    {color}
+                </span>
+                .
+            </div>
         </div>
     );
 }
--- src/form-components/MultipleChoiceQuestion.tsx
+++ src/form-components/MultipleChoiceQuestion.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function MultipleChoiceQuestion({
     options,
@@ -7,9 +8,31 @@ export function MultipleChoiceQuestion({
     options: string[];
     expectedAnswer: string;
 }): JSX.Element {
+    const options2 = [...options];
+    const DEFAULT_OPTIONS = options[0];
+
+    const [choice, setChoice] = useState<string>(DEFAULT_OPTIONS);
+
+    function updateChoice(event: React.ChangeEvent<HTMLSelectElement>) {
+        setChoice(event.target.value);
+    }
+
     return (
         <div>
-            <h3>Multiple Choice Question</h3>
+            <div>
+                <h3>Multiple Choice Question</h3>
+            </div>
+            <Form.Group controlId="userChoice">
+                <Form.Label>Choose an answer:</Form.Label>
+                <Form.Select value={choice} onChange={updateChoice}>
+                    {options2.map((option: string) => (
+                        <option key={option} value={option}>
+                            {option}
+                        </option>
+                    ))}
+                </Form.Select>
+            </Form.Group>
+            {choice === expectedAnswer ? "✔️" : "❌"}
         </div>
     );
 }
